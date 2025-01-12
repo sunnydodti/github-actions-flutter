9962f9a9e3cddfe47ce1dcad50995435b1ad91b5
sunnydodti <sunnydodti@gmail.com>
2025-01-12 17:54:16 +0530

    upgrade version

--- flutter_app/lib/main_dev.dart
+++ flutter_app/lib/main_dev.dart
@@ -6,8 +6,11 @@ import 'base/models/flavour_config.dart';
 import 'base/models/flavour_values.dart';
 
 void main() {
-  FlavorConfig(flavor: Flavor.DEV,
+  FlavorConfig(
+      flavor: Flavor.DEV,
       color: Colors.green,
-      values: FlavorValues(baseUrl: "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_dev.dart"));
+      values: FlavorValues(
+          baseUrl:
+              "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_dev.dart"));
   runApp(const MyApp());
 }
--- flutter_app/lib/main_stg.dart
+++ flutter_app/lib/main_stg.dart
@@ -6,8 +6,11 @@ import 'base/models/flavour_config.dart';
 import 'base/models/flavour_values.dart';
 
 void main() {
-  FlavorConfig(flavor: Flavor.STG,
+  FlavorConfig(
+      flavor: Flavor.STG,
       color: Colors.blue,
-      values: FlavorValues(baseUrl: "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_qa.json"));
+      values: FlavorValues(
+          baseUrl:
+              "https://raw.githubusercontent.com/JHBitencourt/ready_to_go/master/lib/json/person_qa.json"));
   runApp(const MyApp());
 }
